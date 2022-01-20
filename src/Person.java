aecac4e5224fbfa3145e8fa940675cf43502e2ea
johanna <johannautter@gmail.com>
2022-01-20 16:35:36 +0100

    Persons class with constructor

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,15 @@
 public class Person {
+    String firstName;
+    String lastName;
+    int age;
 
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public void speak(){
+        System.out.println("Hello my name is " +this.firstName+ " " +this.lastName);
+    }
 }
